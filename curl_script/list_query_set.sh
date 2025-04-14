9daf176ef7e904ec38f6f72a786979419f1aaa73
Fen Qin <mfenqin@amazon.com>
2025-04-14 09:21:53 -0700

    fix sort field for query_sets and search_configs scripts
    Signed-off-by: Fen Qin <mfenqin@amazon.com>

--- curl_script/list_query_set.sh
+++ curl_script/list_query_set.sh
@@ -2,7 +2,7 @@ curl -s -X GET "localhost:9200/_plugins/search_relevance/query_sets" \
 -H "Content-type: application/json" \
 -d'{
      "sort": {
-       "sampling": {
+       "timestamp": {
          "order": "desc"
        }
      },
--- curl_script/list_search_configuration.sh
+++ curl_script/list_search_configuration.sh
@@ -2,7 +2,7 @@ curl -s -X GET "http://localhost:9200/_plugins/search_relevance/search_configura
 -H "Content-type: application/json" \
 -d'{
      "sort": {
-       "sampling": {
+       "timestamp": {
          "order": "desc"
        }
      },
