bb9f8cb49b74d656e0d640bd31bc27ca267544c8
Alexey Rodriguez Yakushev <alexey.rodriguez@gmail.com>
2025-04-14 18:24:57 +0200

    Fix sortkey in listing Search Configurations
    Signed-off-by: Alexey Rodriguez Yakushev <alexey.rodriguez@gmail.com>

--- curl_script/demo_runbook.sh
+++ curl_script/demo_runbook.sh
@@ -40,7 +40,7 @@ exe curl -s -X GET "http://localhost:9200/_plugins/search_relevance/search_confi
 -H "Content-type: application/json" \
 -d'{
      "sort": {
-       "sampling": {
+       "timestamp": {
          "order": "desc"
        }
      },
