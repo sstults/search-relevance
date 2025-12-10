a429daa6db08fde233f5ecdfca98688c4bfd9067
Scott Stults <sstults@opensourceconnections.com>
2025-12-10 13:00:37 -0500

    test: check system index has auto_expand_replicas set after plugin creates it
    Signed-off-by: Scott Stults <sstults@opensourceconnections.com>

--- src/test/java/org/opensearch/searchrelevance/integration/AutoExpandReplicasIT.java
+++ src/test/java/org/opensearch/searchrelevance/integration/AutoExpandReplicasIT.java
@@ -13,7 +13,6 @@ import java.util.Map;
 
 import org.junit.Test;
 import org.opensearch.client.Response;
-import org.opensearch.client.RestClient;
 import org.opensearch.searchrelevance.BaseSearchRelevanceIT;
 
 public class AutoExpandReplicasIT extends BaseSearchRelevanceIT {
@@ -21,20 +20,27 @@ public class AutoExpandReplicasIT extends BaseSearchRelevanceIT {
     @Test
     public void testAutoExpandReplicasSettingPresent() throws Exception {
         // Create an index via plugin flow by creating a search configuration that references it
-        String indexName = "test-auto-expand-replicas-index";
+        String userIndexName = "test-auto-expand-replicas-index";
         String template = readTemplate("src/test/resources/searchconfig/CreateSearchConfigurationSimpleMatch.json");
-        String body = template.replace("{{index_name}}", indexName);
+        String body = template.replace("{{index_name}}", userIndexName);
 
-        // call plugin to create a search configuration which will trigger index creation
-        Response resp = makeRequest(client(), "PUT", "/_plugins/_search_relevance/search_configurations/" + "test-config",
-            null, toHttpEntity(body), null);
+        // call plugin to create a search configuration which will trigger the plugin's system index creation
+        Response resp = makeRequest(
+            client(),
+            "PUT",
+            "/_plugins/_search_relevance/search_configurations/" + "test-config",
+            null,
+            toHttpEntity(body),
+            null
+        );
         assertEquals(200, resp.getStatusLine().getStatusCode());
 
-        // fetch index settings
-        Response settingsResp = makeRequest(client(), "GET", "/" + indexName + "/_settings", null, null, null);
+        // fetch the plugin system index settings that should be created by the plugin
+        String systemIndex = org.opensearch.searchrelevance.common.PluginConstants.SEARCH_CONFIGURATION_INDEX;
+        Response settingsResp = makeRequest(client(), "GET", "/" + systemIndex + "/_settings", null, null, null);
         Map<String, Object> settingsMap = convertToMap(settingsResp);
         @SuppressWarnings("unchecked")
-        Map<String, Object> indexSettings = (Map<String, Object>) ((Map<String, Object>) settingsMap.get(indexName)).get("settings");
+        Map<String, Object> indexSettings = (Map<String, Object>) ((Map<String, Object>) settingsMap.get(systemIndex)).get("settings");
         @SuppressWarnings("unchecked")
         Map<String, Object> index = (Map<String, Object>) indexSettings.get("index");
         assertEquals("0-1", index.get("auto_expand_replicas").toString());
@@ -48,8 +54,9 @@ public class AutoExpandReplicasIT extends BaseSearchRelevanceIT {
     @SuppressWarnings("unchecked")
     private Map<String, Object> convertToMap(Response response) throws Exception {
         String json = org.apache.http.util.EntityUtils.toString(response.getEntity());
-        return (Map<String, Object>) org.opensearch.common.xcontent.json.JsonXContent.jsonXContent
-            .createParser(org.opensearch.core.xcontent.NamedXContentRegistry.EMPTY, json)
-            .map();
+        return (Map<String, Object>) org.opensearch.common.xcontent.json.JsonXContent.jsonXContent.createParser(
+            org.opensearch.core.xcontent.NamedXContentRegistry.EMPTY,
+            json
+        ).map();
     }
 }
